5fdece6a8d0bb777b368b006d224cfc39610375e
Heba <40660646+hobaDevHome@users.noreply.github.com>
2024-10-09 23:45:27 +0300

    list page

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,27 +1,156 @@
 import {
+  IonAvatar,
+  IonButton,
   IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonChip,
   IonContent,
+  IonDatetime,
+  IonFab,
+  IonFabButton,
   IonHeader,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonLabel,
   IonMenuButton,
+  IonModal,
   IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonSearchbar,
+  IonSegment,
+  IonSegmentButton,
+  IonSkeletonText,
   IonTitle,
   IonToolbar,
+  useIonAlert,
+  useIonToast,
+  useIonViewWillEnter,
 } from "@ionic/react";
-import React from "react";
+import { addOutline, trashBinOutline } from "ionicons/icons";
+import React, { useEffect, useRef, useState } from "react";
 import "./List.css";
 
 const List: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [users, setUsers] = useState<any[]>([]);
+  const [showAlert] = useIonAlert();
+  const [showToast] = useIonToast();
+  const [selectedUser, setSelectedUser] = useState<any>(null);
+  const modal = useRef<HTMLIonModalElement>(null);
+  const cardModal = useRef<HTMLIonModalElement>(null);
+  const [presentingElement, setPresentingElement] =
+    useState<HTMLElement | null>(null);
+  const page = useRef(null);
+  const [activeSegment, setActiveSegment] = useState<any>("details");
+  useIonViewWillEnter(async () => {
+    const users = await getUsers();
+    console.log("🚀 ~ file: List.tsx:10 ~ useIonViewWillEnter ~ users:", users);
+    setUsers(users);
+    setLoading(false);
+  });
+  const getUsers = async () => {
+    const data = await fetch("https://randomuser.me/api?results=10");
+    const users = await data.json();
+    return users.results;
+  };
+  const clearList = () => {
+    showAlert({
+      header: "Confirm!",
+      message: "Are you sure you want to delete all users?",
+      buttons: [
+        { text: "Cancel", role: "cancel" },
+        {
+          text: "Delete",
+          handler: () => {
+            setUsers([]);
+            showToast({
+              message: "All users deleted",
+              duration: 2000,
+              color: "danger",
+            });
+          },
+        },
+      ],
+    });
+  };
+
+  const doRefresh = async (event: any) => {
+    const data = await getUsers();
+    setUsers(data);
+    event.detail.complete();
+  };
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color={"success"}>
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
           <IonTitle>List</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={clearList}>
+              <IonIcon
+                slot="icon-only"
+                icon={trashBinOutline}
+                color={"light"}
+              />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+
+        <IonToolbar color={"success"}>
+          <IonSearchbar />
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+      <IonContent className="ion-padding">
+        <IonRefresher slot="fixed" onIonRefresh={(ev) => doRefresh(ev)}>
+          <IonRefresherContent />
+        </IonRefresher>
+
+        {loading &&
+          [...Array(10)].map((_, index) => (
+            <IonCard key={index}>
+              <IonCardContent className="ion-no-padding">
+                <IonItem lines="none">
+                  <IonAvatar slot="start">
+                    <IonSkeletonText />
+                  </IonAvatar>
+                  <IonLabel>
+                    <IonSkeletonText animated style={{ width: "150px" }} />
+                    <p>
+                      <IonSkeletonText />
+                    </p>
+                  </IonLabel>
+                  <IonChip slot="end" color={"primary"}></IonChip>
+                </IonItem>
+              </IonCardContent>
+            </IonCard>
+          ))}
+
+        {users.map((user, index) => (
+          <IonCard key={index} onClick={() => setSelectedUser(user)}>
+            <IonCardContent className="ion-no-padding">
+              <IonItem lines="none">
+                <IonAvatar slot="start">
+                  <IonImg src={user.picture.large} />
+                </IonAvatar>
+                <IonLabel>
+                  {user.name.first} {user.name.last}
+                  <p>{user.email}</p>
+                </IonLabel>
+                <IonChip slot="end" color={"primary"}>
+                  {user.nat}
+                </IonChip>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+        ))}
+      </IonContent>
     </IonPage>
   );
 };
